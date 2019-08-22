a5ac704518b6a605eb6051663fbe70c586a62cbe
Fernando Gunawan <flashshadow32@gmail.com>
2019-08-22 10:43:17 +0700

    sadasd
    asdasd

--- Project Xcode (iOS)/SwiftSwag-Starter/SwiftSwag/Controller/CategoriesVC.swift
+++ Project Xcode (iOS)/SwiftSwag-Starter/SwiftSwag/Controller/CategoriesVC.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class CategoriesVC: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    let category = [
+        Category(categoryName: "hoodies", categoryImage: "hoodies.png"),
+        Category(categoryName: "digital", categoryImage: "digital.png"),
+        Category(categoryName: "hats", categoryImage: "hats.png"),
+        Category(categoryName: "shirts", categoryImage: "shirts.png"),
+    ]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
---
+++ Project Xcode (iOS)/SwiftSwag-Starter/SwiftSwag/Model/Category.swift
@@ -0,0 +1,13 @@
+//
+//  Category.swift
+//  SwiftSwag
+//
+//  Created by Fernando Gunawan on 22/08/19.
+//  Copyright © 2019 David Gunawan. All rights reserved.
+//
+
+import Foundation
+struct Category {
+    var categoryName: String
+    var categoryImage: String
+}
